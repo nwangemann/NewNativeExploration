28eec0aef5eed09710df02693dbec7c11fb8f85b
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-05-11 17:11:50 -0700

    updates state management

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Input } from "react-native";
 
 export default function App() {
+  const [fileInput, setFileInput] = useState(null)
+  
   return (
     <View style={styles.container}>
       <View style={styles.card}>
